7ffbd69937cfdd68ef7e1ef7e0edd9ed4e35cace
Jax <42078529+Jaxii@users.noreply.github.com>
2022-08-08 21:26:15 -0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #![feature(iterator_try_collect)]
 
+use anyhow::Context;
 use std::error::Error;
-use std::{fmt, fs};
 use std::fmt::Formatter;
 use std::io::{Read, Write};
 use std::net::Ipv4Addr;
-use std::str::{FromStr};
+use std::str::FromStr;
 use std::time::Duration;
-use anyhow::Context;
+use std::{fmt, fs};
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, BufReader};
 type Err = anyhow::Error;
 
@@ -18,14 +18,13 @@ pub struct Proxy {
     pub port: u16,
 }
 
-
 #[derive(Debug, Clone)]
 pub enum ProxyType {
     socks5,
     socks4,
     https,
     http,
-    none
+    none,
 }
 
 impl FromStr for Proxy {
@@ -37,7 +36,7 @@ impl FromStr for Proxy {
             Some("https") => ProxyType::https,
             Some("socks4") => ProxyType::socks4,
             Some("socks5") => ProxyType::socks5,
-            _ => ProxyType::none
+            _ => ProxyType::none,
         };
         let ip: Ipv4Addr = split.next().context("invalid format")?.parse()?;
         let port: u16 = split.next().context("invalid format")?.parse()?;
@@ -56,7 +55,7 @@ impl fmt::Display for ProxyType {
             ProxyType::socks4 => write!(f, "socks4"),
             ProxyType::http => write!(f, "http"),
             ProxyType::https => write!(f, "https"),
-            ProxyType::none => write!(f, "https")
+            ProxyType::none => write!(f, "https"),
         }
     }
 }
@@ -70,8 +69,7 @@ impl fmt::Display for Proxy {
 #[tokio::main]
 async fn main() {
 
-   // load_list("input.txt");
-
+    // load_list("input.txt");
 }
 async fn check_proxy(p: Proxy, timeout: u8, target: &String) -> Result<(), reqwest::Error> {
     let proxy = reqwest::Proxy::all(p.proxy_type.to_string())?;
@@ -79,9 +77,13 @@ async fn check_proxy(p: Proxy, timeout: u8, target: &String) -> Result<(), reqwe
         .danger_accept_invalid_certs(true)
         .proxy(proxy)
         .build()?;
-    client.get(target)
+    client
+        .get(target)
         .header("Accept", "text/plain")
-        .header("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0")
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:35.0) Gecko/20100101 Firefox/35.0",
+        )
         .timeout(Duration::from_secs(timeout as u64))
         .send()
         .await?
@@ -90,10 +92,18 @@ async fn check_proxy(p: Proxy, timeout: u8, target: &String) -> Result<(), reqwe
     Ok(())
 }
 
-async fn load_list(path: &str) -> (Vec<String>) {
+async fn load_list(path: &str) -> Vec<String> {
     let mut f = fs::File::open(path).expect("File not found");
     let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("Could not read file");
+    f.read_to_string(&mut contents)
+        .expect("Could not read file");
 
-    Ok::<Vec<String>, Box<dyn Error>>(contents.as_str().split("\n").map(|x| x.to_string()).collect::<Vec<String>>()).unwrap()
+    Ok::<Vec<String>, Box<dyn Error>>(
+        contents
+            .as_str()
+            .split("\n")
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>(),
+    )
+    .unwrap()
 }
